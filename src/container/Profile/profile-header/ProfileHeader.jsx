d4f54b4b0970230a776857502f61dc7df04d8fd5
Farid <mohammadizadeh79@gmail.com>
2022-08-25 19:55:29 +0430

    profile header total refactored

--- src/container/Profile/profile-header/ProfileHeader.jsx
+++ src/container/Profile/profile-header/ProfileHeader.jsx
@@ -1,37 +1,60 @@
 import React from "react";
 import "./profileHeader.css";
 import { profileContent } from "../data/profiles.js";
-import avatar from "../../../assets/avatar.jpg";
-import banner from "../../../assets/header.jpg";
-
-
+// import avatar from "../../../assets/avatar.jpg";
+// import banner from "../../../assets/header.jpg";
 
 const userID = profileContent[0].ID;
+
+const ProfileBanner = (props) => {
+  return (
+    <>
+      <img
+        className="profile__container__main-banner-image"
+        src={props.profileBanner}
+        alt="banner"
+      />
+    </>
+  );
+};
+
+const ProfileImage = (props) => {
+  return (
+    <>
+      <img
+        className="profile__container__main-content-avatar"
+        src={props.profileAvatar}
+        alt="user"
+      />
+    </>
+  );
+};
 
 const ProfileHeader = () => {
   return (
     <div>
-      <img src="../../../assets/avatar.jpg" alt="an" />
       <div className="profile__container">
         <div className="profile__container__main">
-          <div
-            className="profile__container__main-banner"
-            style={{ backgroundImage: `url(${banner})` }}
-          ></div>
+          <div className="profile__container__main-banner">
+            <ProfileBanner profileBanner={profileContent[userID].banner} />
+          </div>
           <div className="profile__container__main-content">
-            <div
-              className="profile__container__main-content-avatar"
-              style={{ backgroundImage: `url(${avatar})` }}
-            ></div>
+            <div>
+              <ProfileImage profileAvatar={profileContent[userID].avatar} />
+            </div>
             <div className="profile__container__main-content-top">
               <div className="profile__container__main-content-top-name">
-                <h2>{profileContent[userID].firstName} {profileContent[userID].lastName}</h2>
+                <h2>
+                  {profileContent[userID].firstName}{" "}
+                  {profileContent[userID].lastName}
+                </h2>
                 <h3>{profileContent[userID].specialty}</h3>
               </div>
               <div className="profile__container__main-content-top-pricing">
                 <button>درخواست منتورینگ</button>
                 <div className="profile__container__main-content-top-pricing-content">
-                  هر ساعت منتورینگ: <span>{profileContent[userID].priceEachHour}</span>
+                  هر ساعت منتورینگ:{" "}
+                  <span>{profileContent[userID].priceEachHour}</span>
                 </div>
               </div>
             </div>
